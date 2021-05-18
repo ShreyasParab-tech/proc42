b6ac1087a04e328971842391935a426cae2e2981
ShreyasParab-tech <76586923+ShreyasParab-tech@users.noreply.github.com>
2021-05-18 22:52:27 +0530

    Add files via upload

---
+++ PRO-C42-template-main/Drop.js
@@ -0,0 +1,26 @@
+class drop{
+    constructor(x, y) {
+        var options = {
+            friction : 0.001,
+            restitution : 0.1        
+           
+        }
+        this.rain = Bodies.circle(x, y, 9, options);
+        this.radius = 9;
+        World.add(world, this.rain);
+      }
+
+updateY(){
+  if(this.rain.position.y>height){
+    Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)});
+  }
+}
+
+
+
+      showDrop(){
+        fill("blue");
+        ellipseMode(CENTER)
+        ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius)
+      }
+}
